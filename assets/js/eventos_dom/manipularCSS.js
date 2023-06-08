8d12afbb051b13cdf1c0d40c88422c18f55df922
Matheus André Bezerra Mota <matheus.mat96@hotmail.com>
2023-06-07 21:22:27 -0300

    Aprendizado JavaScript

---
+++ assets/js/eventos_dom/manipularCSS.js
@@ -0,0 +1,10 @@
+function manipularCSS() {
+    let css = document.querySelector('.manipularCSS')
+
+    css.style.backgroundColor = "yellow"
+    css.style.border = "1px solid black"
+    css.style.padding = "5px"    
+    css.style.boxShadow = "1px 1px 5px black"
+
+    console.log("Elemento Alterado")
+}
